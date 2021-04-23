11b36e5e75e79771468aca6d2fc35cd597b9fcca
Stephanie Ko <stephanieko@protonmail.com>
2021-04-23 12:13:11 +0100

    Rename variables

--- src/lib/colors.js
+++ src/lib/colors.js
@@ -1,15 +1,15 @@
 import Values from 'values.js';
 
-const swatch = (defaultColor) => {
-  const colors = new Values(defaultColor);
+const colors = (defaultColor) => {
+  const model = new Values(defaultColor);
   const weight = 10; // TODO: future functionality
 
   function setBaseColor(base) {
-    return colors.setColor(base).hex;
+    return model.setColor(base).hex;
   }
 
   function getColorSwatch() {
-    return colors.all(weight).map(({ type, hex }) => ({
+    return model.all(weight).map(({ type, hex }) => ({
       type, weight, hex,
     }));
   }
@@ -20,4 +20,4 @@ const swatch = (defaultColor) => {
   };
 };
 
-export default swatch;
+export default colors;
