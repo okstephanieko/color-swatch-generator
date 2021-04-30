0efc1485eaebc41268c97e0e66e53403ceeb97ba
Stephanie Ko <stephanieko@protonmail.com>
2021-04-30 14:06:42 +0100

    Add color picker component

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,7 +1,7 @@
 import { html } from 'lit-html';
 
-const AppLayout = ({ Header, Content }) => html`<div class="container">
-  <header>${Header}</header>
+const AppLayout = ({ Header, Content }) => html`<div class="container--fluid">
+  <header><sl-color-picker></sl-color-picker></sl-icon>${Header}</header>
   <section>${Content}</section>
 </div>
 `;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import { render } from 'lit-html';
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
+import '@shoelace-style/shoelace/dist/components/button/button';
+import '@shoelace-style/shoelace/dist/components/dropdown/dropdown';
+import '@shoelace-style/shoelace/dist/components/icon/icon';
+import '@shoelace-style/shoelace/dist/components/input/input';
+import '@shoelace-style/shoelace/dist/components/spinner/spinner';
+import '@shoelace-style/shoelace/dist/components/color-picker/color-picker';
 
 import App from './components/App';
 
