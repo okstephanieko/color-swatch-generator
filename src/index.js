d12b2a9e6d8885629bb5b8d7712f6187a4747d49
Stephanie Ko <stephanieko@protonmail.com>
2021-04-30 12:36:02 +0100

    Set up base styles

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import { render } from 'lit-html';
+import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path';
 
 import App from './components/App';
 
-import './styles.css';
+import './styles.scss';
+
+setBasePath('/dist/shoelace');
 
 render(App(), document.getElementById('app'));
